9c79cd1493da4d81df46af477913b47c29d82a67
Gabriel S. <thesouzagab@gmail.com>
2023-10-23 16:50:40 -0300

    feat: add home w/o styling and auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,20 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "public#index"
+
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  resources :sessions, only: %i[index show destroy]
+  resource  :password, only: %i[edit update]
+
+  namespace :identity do
+    resource :email,              only: %i[edit update]
+    resource :email_verification, only: %i[show create]
+    resource :password_reset,     only: %i[new edit create update]
+  end
+
+  resource :invitation, only: %i[new create]
 end
