336e06a963b016a0d546e0d6d26de583e81a0129
Gabriel S. <thesouzagab@gmail.com>
2023-10-26 16:50:25 -0300

    chore: remove request id from current attributes

--- app/controllers/concerns/set_current_request_details.rb
+++ app/controllers/concerns/set_current_request_details.rb
@@ -3,7 +3,6 @@ module SetCurrentRequestDetails
 
   included do
     before_action do
-      Current.request_id = request.uuid
       Current.user_agent = request.user_agent
       Current.ip_address = request.ip
     end
--- app/models/current.rb
+++ app/models/current.rb
@@ -1,6 +1,6 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
-  attribute :request_id, :user_agent, :ip_address
+  attribute :user_agent, :ip_address
 
   delegate :user, to: :session, allow_nil: true
 end
